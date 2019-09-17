91953b57a02d22838af71cd020f6924f3a2fc34b
Michele La Ferrara <michele.laferrara@scai.intra>
2019-09-17 15:34:55 +0200

    Committo inizio sviluppi PARTE 3

--- scai.digitarch/src/main/java/it/scai/digitarch/App.java
+++ scai.digitarch/src/main/java/it/scai/digitarch/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Inizio sviluppi PARTE 2 branch feature_vairable_name" );
+        System.out.println( "Inizio sviluppi PARET 3 branch feature_vairable_name" );
     }
 }

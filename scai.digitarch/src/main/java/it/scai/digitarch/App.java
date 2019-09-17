3d1b45646fcff6de43c80ad024c0e6c27268720a
Michele La Ferrara <michele.laferrara@scai.intra>
2019-09-17 14:55:11 +0200

    Secondo commit di prova del progetto con cambio testo hello world

--- scai.digitarch/src/main/java/it/scai/digitarch/App.java
+++ scai.digitarch/src/main/java/it/scai/digitarch/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Benvenuti nella nuova architettura digitale Intes San Paolo !" );
+        System.out.println( "Benvenuti nella nuova architettura digitale Intes San Giovanni !" );
     }
 }

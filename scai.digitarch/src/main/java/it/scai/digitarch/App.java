9559a89f20dfe0e5da1b8f7129f989a2b3e16817
Michele La Ferrara <michele.laferrara@scai.intra>
2019-09-17 13:18:47 +0200

    Primo commit iniziale del progetto

---
+++ scai.digitarch/src/main/java/it/scai/digitarch/App.java
@@ -0,0 +1,13 @@
+package it.scai.digitarch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Benvenuti nella nuova architettura digitale Intes San Paolo !" );
+    }
+}

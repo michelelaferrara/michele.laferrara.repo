3c9c8bb1d0b140852797352b78264513439b201b
Michele La Ferrara <michele.laferrara@scai.intra>
2019-09-17 15:30:11 +0200

    Committo sul branch l' inizio degli sviluppi

--- scai.digitarch/src/main/java/it/scai/digitarch/App.java
+++ scai.digitarch/src/main/java/it/scai/digitarch/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Benvenuti nella nuova architettura digitale Intes San Giovanni Branch feature_variable_branch !" );
+        System.out.println( "Inizio sviluppi branch feature_vairable_name" );
     }
 }
